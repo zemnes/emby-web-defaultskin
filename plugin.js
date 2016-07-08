00449518910f4209bda440cdd5901b3bb2caba59
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-07-08 17:07:06 -0400

    tizen/opera tv fixes, and update translations

--- plugin.js
+++ plugin.js
@@ -32,20 +32,16 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
             var list = [
                 // Used for the mpaa rating
                 'css!' + pluginManager.mapPath(self, 'css/style'),
-                'css!' + pluginManager.mapPath(self, 'css/colors.dark'),
-                'css!' + pluginManager.mapPath(self, 'css/papericonbutton')
+                'css!' + pluginManager.mapPath(self, 'css/colors.dark')
             ];
 
             if (browser.android) {
                 // on android we can't just use Roboto by name, it has to be sans-serif, which we don't want on other platforms
                 list.push('css!' + pluginManager.mapPath(self, 'css/fonts.android'));
-            } else if (browser.tv && browser.operaTv) {
+            } else if (browser.tv && !browser.chrome) {
                 console.log("Using system fonts with explicit sizes");
                 list.push('css!' + pluginManager.mapPath(self, 'css/fonts.sized'));
-            } else if (browser.tv && (!browser.chrome || browser.web0s)) {
-                // Need to set our own font sizes on web0s
-                list.push('css!' + pluginManager.mapPath(self, 'css/fonts.sized'));
-            } else if (browser.tv && browser.chrome) {
+            } else if (browser.tv) {
                 // Designed to use system default fonts
                 console.log("Using system fonts");
                 list.push('css!' + pluginManager.mapPath(self, 'css/fonts.device'));
