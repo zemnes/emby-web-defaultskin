26e0ba7dda02ab3d4ec7126553be22b637948e47
Aristarchus-BC <Prairie6@yandex.com>
2017-04-23 22:32:08 +1000

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Scenes first Skin';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'emby.testskin';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
